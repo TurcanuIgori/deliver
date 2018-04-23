1345e3b1f6dc594a9420a78ae1de82aa10509622
iturcanu <tzurcanu93@gmail.com>
2018-04-23 22:32:08 +0300

    feature/DELIVERY-16 generated table in pdf report

--- src/main/java/md/delivery/controller/rest/CommandReportController.java
+++ src/main/java/md/delivery/controller/rest/CommandReportController.java
@@ -1,13 +1,17 @@
 package md.delivery.controller.rest;
 
 import com.itextpdf.text.DocumentException;
-import md.delivery.reports.GeneratePdfReport;
+import md.delivery.reports.GeneratePdfReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,9 +22,12 @@ import java.io.ByteArrayInputStream;
 @PreAuthorize("isFullyAuthenticated()")
 public class CommandReportController {
 
+    @Autowired
+    private GeneratePdfReportService generatePdfReportService;
+
     @GetMapping(value = "/generateReportForCommand/{commandId}", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> test(@PathVariable("commandId") Long commandId, HttpServletRequest request, HttpServletResponse response) throws DocumentException {
-        ByteArrayInputStream bis = GeneratePdfReport.commandReport(commandId);
+        ByteArrayInputStream bis = generatePdfReportService.commandReport(commandId);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
